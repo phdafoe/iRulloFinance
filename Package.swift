ae90e0a3b804a1d9a8c96fbcdf5d8fd5c5038853
phdafoe <phdafoe@gmail.com>
2024-09-19 13:09:11 +0200

    [WIP] - added logic framework

--- Package.swift
+++ Package.swift
@@ -5,19 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "iRulloFinance",
+    platforms: [.macOS(.v12), .iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "iRulloFinance",
             targets: ["iRulloFinance"]),
+        .executable(name: "iRulloFinanceExc",
+                    targets: ["iRulloFinanceExc"])
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "iRulloFinance"),
-        .testTarget(
-            name: "iRulloFinanceTests",
-            dependencies: ["iRulloFinance"]),
+            name: "iRulloFinance",
+            dependencies: []),
+        .executableTarget(name: "iRulloFinanceExc",
+                          dependencies: ["iRulloFinance"])
     ]
 )
---
+++ Sources/iRulloFinanceExc/iRulloFinanceExc.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Andres Felipe Ocampo Eljaiek on 19/9/24.
+//
+
+import Foundation
